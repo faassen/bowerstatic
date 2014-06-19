e8777a7de5a797809c0634ee59964a0e529408fb
faassen <faassen@startifact.com>
2014-06-19 16:53:10 +0200

    Forgot toposort.

---
+++ bowerstatic/toposort.py
@@ -0,0 +1,22 @@
+class TopologicalSortError(Exception):
+    pass
+
+
+def topological_sort(l, get_depends):
+    result = []
+    marked = set()
+    temporary_marked = set()
+
+    def visit(n):
+        if n in marked:
+            return
+        if n in temporary_marked:
+            raise TopologicalSortError("Not a DAG")
+        temporary_marked.add(n)
+        for m in get_depends(n):
+            visit(m)
+        marked.add(n)
+        result.append(n)
+    for n in l:
+        visit(n)
+    return result
