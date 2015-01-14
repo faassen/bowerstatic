3637806d5d435648ae2dc34a8f9f91ceda4ae2ec
faassen <faassen@startifact.com>
2015-01-14 14:08:19 +0100

    Include render_inline_css and render_inline_js.

--- bowerstatic/__init__.py
+++ bowerstatic/__init__.py
@@ -6,3 +6,4 @@ from .autoversion import (filesystem_second_autoversion,
 from .utility import module_relative_path
 from .publisher import PublisherTween
 from .injector import InjectorTween
+from .renderer import render_inline_js, render_inline_css
