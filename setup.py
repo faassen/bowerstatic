4b4dee860975d52165f5c99528996fa032b9da33
faassen <faassen@startifact.com>
2015-01-14 14:23:02 +0100

    Back to development: 0.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests_require = [
 
 setup(
     name='bowerstatic',
-    version='0.8',
+    version='0.9.dev0',
     description="A Bower-centric static file server for WSGI",
     long_description=long_description,
     author="Martijn Faassen",
